96a7ed9d96de346edd453831720c56def9062392
Xen <klimovjeka0987@gmail.com>
2017-09-23 22:59:16 +0300

    no message

--- panorama/scripts/sa_timer.js
+++ panorama/scripts/sa_timer.js
@@ -68,6 +68,7 @@ function StopTimer()
 		$.CancelScheduled(schedule)
 	schedule = null
 	$("#TimerContainer").AddClass("Hidden")
+	GameEvents.SendCustomGameEventToServer("timer_end", {} );
 }
 
 function NewEndTime(data)
